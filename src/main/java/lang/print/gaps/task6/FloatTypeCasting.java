c877e4f2dfddad5d696713a09f60b0a5b9fb1226
SardorAKayumov <sardor.zeroz@gmail.com>
2022-09-26 15:17:05 +0500

    done

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        //Taking away decimals by casting the type to int
+        int intNumberWithoutDecimals = (int) numberToBeRounded + 1;
+        //returning the number without decimals to float for further calculations
+        float floatNumberWithoutDecimals = (float) intNumberWithoutDecimals - 1;
+
+        int answer = (numberToBeRounded-floatNumberWithoutDecimals) * 10 >= 5 ? (int) numberToBeRounded + 1 : (int) numberToBeRounded;
+        System.out.println(answer);
     }
 }
